0ebc00a4dfa369cc35000bcd9f45fb804c3a3a9c
gupiragupio <suzukikouhei2020@gmail.com>
2024-10-28 14:45:54 +0900

    ルーディング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品が出品されました' # 保存成功時の処理
+    else
+      render :new # 保存失敗時の処理（出品ページに戻す）
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :category_id, :price, :info, :state_id, :fee_id, :time_to_delivery_id,
+                                 :prefecture_id).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'items/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'items#index'
+  resources :items, only: [:new, :create]
 end
 
 
