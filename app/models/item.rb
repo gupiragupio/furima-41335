06fb4d16e907e7fce7a561cd727003ce2bc5e950
gupiragupio <suzukikouhei2020@gmail.com>
2024-10-29 15:57:16 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order_log
   has_one_attached :image
+
+  validates :item_name, presence: true
+  validates :category_id, presence: true
+  validates :price, presence: true
+  validates :info, presence: true
+  validates :state_id, presence: true
+  validates :fee_id, presence: true
+  validates :time_to_delivery_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :image, presence: true
 end
