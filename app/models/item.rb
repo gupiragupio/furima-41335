bf4efc8066d71ad79be63abf401cee88dca3f1bc
gupiragupio <suzukikouhei2020@gmail.com>
2024-11-08 12:39:55 +0900

    価格は半角数値のみ保存可能であること。のバリデーションを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: { only_integer: true, message: 'is not a number' }
   validates_numericality_of :price, greater_than_or_equal_to: 300, message: 'must be greater than or equal to 300'
   validates_numericality_of :price, less_than_or_equal_to: 9_999_999, message: 'must be less than or equal to 9999999'
   validates :info, presence: true
