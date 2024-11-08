5b08b4cf526a5318f2ab434ccab5cba87e469241
gupiragupio <suzukikouhei2020@gmail.com>
2024-11-08 12:33:52 +0900

    価格は、¥300~¥9,999,999の間のみ保存可能であること。のエラーメッセージ実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,8 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true
+  validates_numericality_of :price, greater_than_or_equal_to: 300, message: 'must be greater than or equal to 300'
+  validates_numericality_of :price, less_than_or_equal_to: 9_999_999, message: 'must be less than or equal to 9999999'
   validates :info, presence: true
   validates :state_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
