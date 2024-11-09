19e386e72ac505a4faa09ece8cf7a3c964155c8e
gupiragupio <suzukikouhei2020@gmail.com>
2024-11-09 14:09:14 +0900

    ログインユーザーでないと商品投稿できないテストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,17 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.build(:item) # FactoryBotでItemのテストデータを準備
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user: @user) # FactoryBotでItemのテストデータを準備し、ユーザーを紐づける
   end
 
   describe '商品出品' do
-    context '商品出品ができるとき' do
-      it '必要な情報をすべて正しく入力すれば出品できる' do
+    context '出品がうまくいくとき' do
+      it '全ての項目が正しく入力されていれば出品できる' do
         expect(@item).to be_valid
       end
     end
 
-    context '商品出品ができないとき' do
+    context '出品がうまくいかないとき' do
+      it 'ユーザーが紐づいていないと出品できない' do
+        @item.user = nil # ユーザーを紐付けない
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+
       it '商品画像が必須であること' do
         @item.image = nil
         @item.valid?
