cabdafbecfb3d078927193d6f30aa5e61e30a190
gupiragupio <suzukikouhei2020@gmail.com>
2024-11-16 11:30:44 +0900

    コントローラにアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :check_authorization, only: [:edit, :update]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -21,10 +23,33 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+
+  def check_authorization
+    redirect_to root_path unless current_user == @item.user
+  end
+
+  def update
+    if @item.update(item_params) # 編集内容を更新
+      redirect_to item_path(@item), notice: '商品情報を更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :category_id, :price, :info, :state_id, :fee_id, :time_to_delivery_id,
                                  :prefecture_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_authorization
+    redirect_to root_path unless current_user == @item.user
   end
 end
