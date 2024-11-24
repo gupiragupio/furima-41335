77d9f3d1fc7c3c006416be4737f2152b8a4f4a9e
gupiragupio <suzukikouhei2020@gmail.com>
2024-11-24 21:23:11 +0900

    バリデーションの記述をリファクタリング

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,11 +2,13 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id, :token
 
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :city, :street, :phone_number, :user_id, :item_id, :token
+  end
+
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city, :street, :phone_number, :user_id, :item_id, presence: true
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
-  validates :token, presence: true
 
   def save
     order_log = OrderLog.create(user_id: user_id, item_id: item_id)
