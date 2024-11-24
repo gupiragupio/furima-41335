d0e39477d6436da99466fecb1145a412faafdd85
gupiragupio <suzukikouhei2020@gmail.com>
2024-11-24 16:06:06 +0900

    エラーメッセージ表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,16 +5,27 @@ class OrdersController < ApplicationController
 
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
-    redirect_to root_path, alert: 'この商品はすでに購入されています。' if @item.order_log.present?
     @order = OrderForm.new
   end
 
   def create
     @order = OrderForm.new(order_params)
+    if order_params[:token].blank?
+      flash.now[:alert] = 'カード情報を入力してください'
+      render :index, status: :unprocessable_entity
+      return
+    end
+
     if @order.valid?
-      pay_item
-      @order.save
-      redirect_to root_path, notice: '購入が完了しました。'
+      begin
+        pay_item
+        @order.save
+        redirect_to root_path, notice: '購入が完了しました。'
+      rescue Payjp::PayjpError => e
+        Rails.logger.error("PayjpError: #{e.message}")
+        flash.now[:alert] = '決済処理に失敗しました。もう一度お試しください。'
+        render :index, status: :unprocessable_entity
+      end
     else
       render :index, status: :unprocessable_entity
     end
@@ -27,11 +38,17 @@ class OrdersController < ApplicationController
   end
 
   def check_authorization
-    redirect_to root_path if current_user == @item.user || @item.order_log.present?
+    if current_user == @item.user
+      redirect_to root_path, alert: '自分の商品は購入できません。'
+    elsif @item.order_log.present?
+      redirect_to root_path, alert: 'この商品はすでに購入されています。'
+    end
   end
 
   def order_params
-    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :street, :building, :phone_number).merge(
+    params.require(:order_form).permit(
+      :postal_code, :prefecture_id, :city, :street, :building, :phone_number
+    ).merge(
       user_id: current_user.id, item_id: @item.id, token: params[:token]
     )
   end
