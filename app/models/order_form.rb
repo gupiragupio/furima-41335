edf16b00e9ef1726873206680c00d80fda844014
gupiragupio <suzukikouhei2020@gmail.com>
2024-11-24 21:15:30 +0900

    :user_id, :item_id,をバリデーションについか

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -4,7 +4,7 @@ class OrderForm
 
   validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city, :street, :phone_number, presence: true
+  validates :city, :street, :phone_number, :user_id, :item_id, presence: true
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
   validates :token, presence: true
 
